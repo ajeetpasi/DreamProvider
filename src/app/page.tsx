fd3cd1af3c0a5ee8353bdb45753c7df348ebe0ad
ajeetpasi <92255855+ajeetpasi@users.noreply.github.com>
2023-09-19 23:52:01 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,10 @@
-import Image from 'next/image'
-import MainCard from './components/cards/Cards'
-import HISonu from '../../../assignment/src/app/TespApi'
 
+import MainCard from './components/cards/Cards'
 
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between ">
       <MainCard />
-      {/* <HISonu /> */}
     </main>
   )
 }
